064aa37146f31453720c2f631e4d39abe5c7aa43
yhcss1 <129813637+yhcss1@users.noreply.github.com>
2024-02-01 20:11:09 +0800

    feat: 拖拽

--- src/views/MockExam/components/DropBox.vue
+++ src/views/MockExam/components/DropBox.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="w-full h-14 border border-green-1 border-solid mb-1.5" :ref="drop"></div>
+  <div 
+    class="w-full h-14 border border-green-1 border-solid mb-1.5" 
+    :ref="drop"
+    :style="style"
+  >
+    <p v-if="typevalue" class="text-gray-500 pt-3 px-4">{{ typevalue }}</p>
+  </div>
 </template>
 <script setup lang="ts">
-import { useDrop, DropTargetMonitor } from 'vue3-dnd'
-import { defineProps, toRefs } from 'vue'
+import { useDrop } from 'vue3-dnd'
+import type { DropTargetMonitor } from 'vue3-dnd'
+import { toRefs } from '@vueuse/core'
+import { computed, defineProps, unref, ref} from 'vue'
+const typevalue = ref<string>('')
 const props = defineProps<{
-  onDrop: (item: any) => void
-  lastDroppedColor?: string
+  onDrop: (item: any) => void,
+  index: number,
+  resource: string,
 }>()
 const [collect, drop] = useDrop(() => ({
   accept: ['A','B','C','D','E','F'],
   drop(_item, monitor) {
-    console.log(monitor.getItemType())
-    // props.onDrop(monitor.getItemType())
+    props.onDrop({
+      type: monitor.getItemType(),
+      index: props.index,
+    })
+    typevalue.value = props.resource[monitor.getItemType()]
     return undefined
   },
-  collect: (monitor: DropTargetMonitor) => ({
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop(),
-    draggingColor: monitor.getItemType() as string,
-  }),
+  collect: (monitor: DropTargetMonitor) => {
+    return {
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }
+  },
 }))
-const { isOver, draggingColor, canDrop } = toRefs(collect)
+const { isOver, canDrop } = toRefs(collect)
+const style = computed(() => {
+  return unref(isOver) ? {
+    backgroundColor: 'rgba(27, 139, 140, 0.05)',
+    border: '1px dashed #1B8B8C',
+  } : {}
+})
 </script>
--- src/views/MockExam/components/ExamLaseMcItem.vue
+++ src/views/MockExam/components/ExamLaseMcItem.vue
@@ -3,7 +3,13 @@
     <h2 class="text-gray-500 text-[16px] pb-3 font-bold">Directions: An introductory sentence for a brief summary of the passage is provided below. Complete the summary by selecting the THREE answer choices that express the most important ideas in the passage. Some answer choices do not belong in the summary because they express ideas that are not presented in the passage or are minor ideas in the passage. This questions is worth 2 points.  Drag your answer choices to the spaces here they belong. To remove an answer choice, click on it. To review the passage, click VIEW TEXT.</h2>
     <h1 class="text-green-1 text-[16px] pb-8 font-bold">{{ props.stem }}</h1>
     <div>
-      <DropBox v-for="val in props.restriction_count" :key="val"></DropBox>
+      <DropBox 
+        v-for="(val, i) in props.restriction_count" 
+        :key="val" 
+        :resource="resource"
+        :onDrop="onDrop"
+        :index="i"
+      ></DropBox>
     </div>
     <h2 class="text-gray-500 text-[16px] font-bold pb-3 pt-4">Answer Choices</h2>
     <div class="flex flex-wrap w-[calc(100%+20px)] -ml-[10px]">
@@ -12,7 +18,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
 import { isMobile } from "@/utils/utils"
 import DropBox from './DropBox.vue'
 import DragBox from './DragBox.vue'
@@ -29,4 +35,13 @@ const props = defineProps<{
     s?: string
   }
 }>()
+const resource = computed(() => {
+  return props.choice.reduce((def, val, i) => {
+    def[props.choice_label[i]] = val
+    return def
+  }, {})
+})
+const onDrop = ({type, index}) => {
+  console.log('onDrop::',type, index)
+}
 </script>
