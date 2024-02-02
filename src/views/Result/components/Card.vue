0f52b4cf37037932d5bcba8454174136f439b594
yhcss1 <129813637+yhcss1@users.noreply.github.com>
2024-02-02 15:15:04 +0800

    style: 模考小结

--- src/views/Result/components/Card.vue
+++ src/views/Result/components/Card.vue
@@ -5,22 +5,77 @@
     </div>
     <span class="absolute right-14 top-6 cursor-pointer text-[#F7A705]">回顾答案<ArrowRightOutlined class="pl-2"/></span>
     <div class="w-[56vw]">
-      <ul>
-
+      <ul class="flex mb-8 mt-2 list">
+        <li v-for="val in summaryList" :key="val.id" class="flex-1 list-none flex items-center justify-center flex-col">
+          <h2 class="text-gray-600 font-normal text-[16px]">{{ val.title }}</h2>
+          <p class="font-bold text-gray-600">
+            <span class="text-[36px]">{{ props[val.id] }}</span><span class="text-[24px]"> / {{ props[`${val.id}Total`] }}</span>
+          </p>
+        </li>
       </ul>
-      <div>
-
+      <div class="flex flex-wrap">
+        <span v-for="(val,i) in props.questions" :key="i" class="px-4 py-1.5 rounded-full text-gray-600 bg-[#E7EAEE] text-[16px] mx-2">
+            {{ val.title }}: {{ val.correct }}/{{ val.total }}
+        </span>
       </div>
-      <div :style="{background: 'rgba(208, 240, 230, 0.50)'}" class="rounded-md p-5">1111</div>
+      <div :style="{background: 'rgba(208, 240, 230, 0.50)'}" class="rounded-md p-5 pt-6 mt-12 ai-comment relative">{{props.aiComment}}</div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { ArrowRightOutlined } from '@ant-design/icons-vue'
+import { defineProps } from 'vue';
+const props = defineProps<{
+    aiComment: string,
+    questions: {
+      title: string,
+      total: number,
+      correct: number,
+    }[],
+    mockScoreTotal: number,
+    mockScore: number,
+    basisScoreTotal: number,
+    basisScore: number,
+    intensifyScoreTotal: number,
+    intensifyScore: number,
+    summarySourceTotal: number,
+    summarySource: number,
+}>()
+const summaryList = [
+  {
+    title: '模考得分',
+    id: 'mockScore',
+  },
+  {
+    title: '基础题',
+    id: 'basisScore',
+  },
+  {
+    title: '强化题',
+    id: 'intensifyScore',
+  },
+  {
+    title: '文章小结题',
+    id: 'summarySource',
+  }
+]
 </script>
 <style scoped>
   .result-icon {
     background: url('@/assets/images/result-icon.jpg') no-repeat;
     background-size: 100% 100%;
+  }
+  .ai-comment:before {
+    content: '';
+    position: absolute;
+    width: 120px;
+    height: 32px;
+    background: url('@/assets/images/aititle.png') no-repeat;
+    background-size: cover;
+    top: -18px;
+    left: -20px;
+  }
+  .list li:first-child {
+    border-right: 1px solid #D0D5DD;
   }
 </style>
--- src/views/Result/index.vue
+++ src/views/Result/index.vue
@@ -12,7 +12,7 @@
           <LeftOutlined class="text-green-1 text-[22px]" />
         </div>
         <div>
-          <Card />
+          <Card v-bind="data" />
         </div>
         <div class="w-14 h-14 rounded-full bg-white flex justify-center items-center ml-14 cursor-pointer" :style="{boxShadow: '0px 3px 3px 0 rgb(27 139 140 / 60%)'}">
           <RightOutlined class="text-green-1 text-[22px]" />
@@ -26,9 +26,32 @@
 import { ArrowLeftOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons-vue'
 import { useRouter } from "vue-router"
 import Card from './components/Card.vue'
-import {ref} from "vue"
+import {ref, reactive} from "vue"
 const $router = useRouter()
 const title = ref("模考小结")
+const data = reactive({
+  aiComment: '在使用鹦鹉智学时，您可以随时与AI助教交流。我们深知托福学习的困难与沮丧。所以她不仅是一个经验丰富的托福老师，更是一个可以给您情绪价值的好友，帮您排解托福学习的压力。',
+  questions: [
+    {
+      title: '细节题',
+      total: 3,
+      correct: 2,
+    },
+    {
+      title: '排除题',
+      total: 3,
+      correct: 2,
+    }
+  ],
+  mockScoreTotal: 30,
+  mockScore: 25,
+  basisScoreTotal: 20,
+  basisScore: 15,
+  intensifyScoreTotal: 7,
+  intensifyScore: 7,
+  summarySourceTotal: 3,
+  summarySource: 2,
+})
 const onClickToRead = () => {
   $router.push("/exam/list")
 }
