30bbd7cd27fdaa5fba19a441a05c2bf92a85adec
yhcss1 <129813637+yhcss1@users.noreply.github.com>
2024-02-02 13:58:40 +0800

    feat: 提交答题

--- src/service/exam.ts
+++ src/service/exam.ts
@@ -22,3 +22,22 @@ export const request_startExam = (data: EXAN_START) => {
 export const request_getExam = (id: string) => {
   return http.get(`/api/system/microservice/get_mock/${id}`)
 }
+// 查看答题状态 进度
+export const request_getExamStutas = (id: string) => {
+  return http.get(`/api/system/microservice/answer_status/${id}/`)
+}
+type EXAM_ANSWER = {
+  question_id: number
+  answer: number[]
+  duration: number
+}
+// 保存答案
+export const request_saveAnswer = (data: EXAM_ANSWER) => {
+  return http.post(`/api/system/microservice/answer_panel/`, data)
+}
+// 结束考试提交
+export const request_submitExam = (practice_id: string) => {
+  return http.post(`/api/system/microservice/submit_answers/`, {
+    practice_id
+  })
+}
