4f27bc8b90ffea6a8164018ee21ab97a9d1c7f1b
yhcss1 <129813637+yhcss1@users.noreply.github.com>
2024-02-29 16:52:22 +0800

    showModal
    弹框演示

--- src/views/WordRecite/index.vue
+++ src/views/WordRecite/index.vue
@@ -1,4 +1,11 @@
 <template>
+  <div v-if="isJumpWordTest">
+    <a-modal v-model:open="modal2Visible" title="Vertically centered modal dialog" centered @ok="modal2Visible = false">
+      <p>some contents...</p>
+      <p>some contents...</p>
+      <p>some contents...</p>
+    </a-modal>
+  </div>
   <div class="flex flex-col w-full h-full p-14 overflow-y-auto">
     <h1 class="font-inter text-3xl font-semibold leading-9 mb-10">学习记录</h1>
     <div class="grid grid-cols-6 gap-2 h-[140px]">
@@ -29,7 +36,7 @@
         </a-card-meta>
       </a-card>
     </div>
-    <div class="flex-1 grid grid-rows-3 grid-cols-9 gap-2 mt-12">
+    <div class="flex-1 grid grid-rows-3 grid-cols-9 gap-4 mt-12">
       <a-card class="col-start-1 col-span-5 row-span-3 shadow-lg">
         1
       </a-card>
@@ -57,5 +64,12 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue'
 const $router = useRouter()
+const isJumpWordTest = ref<boolean>(true)
+const modal2Visible = ref<boolean>(false);
+
+onMounted(() => {
+  modal2Visible.value = true
+})
 </script>
