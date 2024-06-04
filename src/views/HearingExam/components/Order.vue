2b5ac0bb7eebad74a05294299a848f11f796794c
hangyi <343043880@qq.com>
2024-06-04 14:15:07 +0800

    fix: move

--- src/views/HearingExam/components/Order.vue
+++ src/views/HearingExam/components/Order.vue
@@ -2,43 +2,46 @@
   <div class="px-32">
     <h2 class="pt-16 pb-14 text-[#475467] text-xl">{{ props.question_content }}</h2>
     <div>
-      <DropBox 
-        v-for="(val, i) in props.answer" 
-        :key="i" 
+      <DropBox
+        v-for="(val, i) in props.answer"
+        :key="i"
         :checked="res"
-        :resource="resource" 
-        :onDrop="onDrop" 
+        :resource="resource"
+        :onDrop="onDrop"
         :onDel="onDel"
-        :accept="['0', '1', '2']"
+        :accept="props.answer.map((val, i) => `${i}`)"
         :index="i"
       ></DropBox>
     </div>
     <h2 class="text-gray-500 text-[16px] font-bold pb-3 pt-4">Answer Choices</h2>
     <div class="flex flex-wrap w-[calc(100%+20px)] -ml-[10px]">
-      <DragBox 
-        v-for="(val, i) in props.detail" 
-        :key="i" 
-        :val="val" 
+      <DragBox
+        v-for="(val, i) in props.detail"
+        :key="i"
+        :val="val"
         :type="`${i}`"
         :res="res"
       ></DragBox>
     </div>
-    <div class="py-2 px-4 mt-4 bg-[#F6F6F6] text-[#475467] text-base">Directions: Drag your answer choices to the spaces here they belong. To remove an answer choice, click on it. To review the passage, click VIEW TEXT.</div>
+    <div class="py-2 px-4 mt-4 bg-[#F6F6F6] text-[#475467] text-base">
+      Directions: Drag your answer choices to the spaces here they belong. To remove an answer
+      choice, click on it. To review the passage, click VIEW TEXT.
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { useHearingExam } from "@/stores/hearingExam";
-import DragBox from '@/views/ReadExam/components/DragBox.vue';
-import DropBox from '@/views/ReadExam/components/DropBox.vue';
-import { computed, defineProps, ref, watchEffect } from 'vue';
+import { useHearingExam } from '@/stores/hearingExam'
+import DragBox from '@/views/ReadExam/components/DragBox.vue'
+import DropBox from '@/views/ReadExam/components/DropBox.vue'
+import { computed, defineProps, ref, watchEffect } from 'vue'
 const examStore = useHearingExam()
 const res = ref<any[]>([])
 const props = defineProps<{
-  question_title: string;
-  detail: string[];
-  question_id: number;
-  options_label: string[];
-  question_content: string;
+  question_title: string
+  detail: string[]
+  question_id: number
+  options_label: string[]
+  question_content: string
   answer: number[]
 }>()
 const resource = computed(() => {
@@ -48,15 +51,15 @@ const resource = computed(() => {
   }, {})
 })
 watchEffect(() => {
-  const answerValue = examStore?.answerData?.find((val) => val.question_id === props.question_id);
-  const _answer = answerValue?.answer.map(val => `${val}`)
-  if(answerValue?.is_answer) {
+  const answerValue = examStore?.answerData?.find((val) => val.question_id === props.question_id)
+  const _answer = answerValue?.answer.map((val) => `${val}`)
+  if (answerValue?.is_answer) {
     res.value = _answer
-  } 
+  }
 })
-const onDrop = ({ type, index }: { type: string, index: number }) => {
+const onDrop = ({ type, index }: { type: string; index: number }) => {
   res.value[index] = type
-  if(res.value.length === props.answer.length) {
+  if (res.value.length === props.answer.length) {
     examStore.saveQuestion(props.question_id, res.value)
   }
 }
@@ -73,8 +76,8 @@ const onDel = ({ index }: { index: number }) => {
   background-color: var(--color-green-1);
 }
 
-.mycheckbox:global(.ant-checkbox+span),
-.myraido:global(.ant-radio-wrapper span.ant-radio+*) {
+.mycheckbox:global(.ant-checkbox + span),
+.myraido:global(.ant-radio-wrapper span.ant-radio + *) {
   flex: 1;
   overflow: hidden;
   display: flex;
